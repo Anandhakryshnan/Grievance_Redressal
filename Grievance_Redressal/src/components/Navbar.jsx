6a3a06842849ac96fe9f9fab09f9d2360877203c
DilshadAhammed <dilshadnakunnath@gmail.com>
2024-09-27 23:24:59 +0530

    navbar started

---
+++ Grievance_Redressal/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./index.css"
+function Navbar() {
+  return (
+    <div className='nav'>
+      <div>
+        <img src="./download.png"  className='kerala-png'/>
+        <h1>Grievance redressal System
+        Industries Department - GoK</h1>
+      </div>
+      <div>
+        <ul>
+            <li>hi</li>
+            <li>cnjcn</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar

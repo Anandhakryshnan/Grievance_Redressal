55ce71208c63e38b42fb7d1873e65824c0dc509d
Anandha Krishnan <Anandhakryshnan@gmail.com>
2024-09-28 22:22:35 +0530

    changed all home

--- Grievance_Redressal/src/components/Navbar.jsx
+++ Grievance_Redressal/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#004b87', padding: '10px 0', color: 'white', textAlign: 'center' }}>
+    <nav style={{ backgroundColor: '#004b88', padding: '10px 0', color: 'white', textAlign: 'center' }}>
       <Link to="/" style={{ color: 'white', marginRight: 20, textDecoration: 'none' }}>Home</Link>
       <Link to="/submit-grievance" style={{ color: 'white', marginRight: 20, textDecoration: 'none' }}>Submit Grievance</Link>
       <Link to="/track-grievance" style={{ color: 'white', textDecoration: 'none' }}>Track Grievance</Link>
